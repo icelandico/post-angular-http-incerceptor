11e944e2739b4af17f077680032b29ea34b16fbe
Michal Muszynski <m.muszy@gmail.com>
2021-07-18 20:01:34 +0200

    Fetch and display data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <div>
+      <h1>Http Interceptor</h1>
+
+      <h2>Regular response:</h2>
+      <pre *ngFor="let user of users" [innerHTML]="user | json"></pre>
+    </div>
+  `,
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-http-interceptor';
+export class AppComponent implements OnInit {
+  public users: any = [];
+
+  constructor(
+    private httpClient: HttpClient
+  ) {
+  }
+
+  ngOnInit() {
+    this.httpClient.get('https://jsonplaceholder.typicode.com/users').subscribe(resp => this.users = resp);
+  }
+
 }
