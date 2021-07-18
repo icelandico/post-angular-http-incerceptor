9b567ea09d6b02f4ce7866f70ab2edbf642a4f5e
Michal Muszynski <m.muszy@gmail.com>
2021-07-18 18:09:03 +0200

    Handle active element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,41 @@
 import { Component } from '@angular/core';
 
+interface IBox {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'example-app';
+  activeElement: string;
+  boxCollection: IBox[] = [
+    {
+      id: 1,
+      name: 'Uruguay'
+    },
+    {
+      id: 2,
+      name: 'Mongolia'
+    },
+    {
+      id: 3,
+      name: 'Japan'
+    },
+    {
+      id: 4,
+      name: 'Moldova'
+    },
+    {
+      id: 5,
+      name: 'Rwanda'
+    }
+  ];
 
+  handleActiveClick(value: string): void {
+    this.activeElement = value;
+  }
 }
